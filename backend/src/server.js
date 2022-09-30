e4c7c5e8671e613aa90d4b746ba23a0ea6683455
henrik-phs <pedrohenriquedasilva100@yahoo.com.br>
2022-09-29 21:17:52 -0300

    db access

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,6 @@ const routes = require('./routes')
 
 const server = express()
 
-// mongoose.connect('mongodb+srv://pedro_admin:rg22pedro@cluster0.abqxl.gcp.mongodb.net/omnistack8?retryWrites=true&w=majority')
 mongoose.connect('mongodb://localhost/omnistack')
 
 server.use(cors())
